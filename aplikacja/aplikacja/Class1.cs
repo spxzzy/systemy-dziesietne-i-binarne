dcbfe67f567b8f26e86b7df873ac69461c5693ac
spxzzy <75693511+spxzzy@users.noreply.github.com>
2023-10-23 16:51:14 +0200

    Add files via upload

---
+++ aplikacja/aplikacja/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aplikacja
+{
+    internal class SystemyLiczbowe : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private int liczbaDz;
+
+        public int LiczbaDz
+        {
+            get { return liczbaDz; }
+            set
+            {
+                liczbaDz = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("LiczbaDz"));
+            }
+        }
+
+        private int wynikB;
+  
+        public int WynikB
+        {
+            get { return wynikB; }
+            set
+            {              
+                wynikB = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("WynikB"));
+            }
+        }
+        private int liczbaB;
+
+        public int LiczbaB
+        {
+            get { return liczbaB; }
+            set
+            {
+                liczbaB = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("LiczbaB"));
+            }
+        }
+        private int wynikDz;
+
+        public int WynikDz
+        {
+            get { return wynikDz; }
+            set
+            {
+                wynikDz = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("WynikDz"));
+            }
+        }
+        public SystemyLiczbowe()
+        {
+            LiczbaDz = 120;
+            LiczbaB = 0101;
+            WynikDz = 0;
+            WynikB = 0;
+        }
+        public void zDznaB()
+        {
+            WynikB = 101010;
+        }
+    }   
+}
+
---
+++ aplikacja/aplikacja/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace aplikacja
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+        public partial class MainWindow : Window
+        {
+        SystemyLiczbowe systemyL = new SystemyLiczbowe();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = systemyL;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Wciśnięto menu kopiuj");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            systemyL.zDznaB();
+        }
+    }
+    }
+
